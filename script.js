228578d7d248a80f61d29cd724a5a7db79196bed
Anthony <124739034+ant444@users.noreply.github.com>
2024-04-05 22:11:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -184,7 +184,17 @@ function resetButtonColors()
 	about_button.style.filter = "brightness(100%)";
 }
 
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach( button =>{
+    button.addEventListener('click',()=>{
+        const faq = button.nextElementSibling;
+        const icon = button.children[1];
+
+        faq.classList.toggle('show');
+        icon.classList.toggle('rotate');
+    })
+} )
 document.addEventListener('DOMContentLoaded', function() {
     var faqQuestions = document.querySelectorAll('.faq-question');
 
